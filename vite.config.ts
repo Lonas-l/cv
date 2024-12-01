f40eb258417c988f829639dc579b71ce97e79057
Maksym Huk <maksym.huk@develops.today>
2024-12-01 15:30:40 +0200

    [Setup] prettier configuration and dev packages

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@lib/*": path.resolve(__dirname, "./lib"),
+      "@constants/*": path.resolve(__dirname, "./constants"),
+      "@context/*": path.resolve(__dirname, "./context"),
+      "@features/*": path.resolve(__dirname, "./features"),
+      "@utils/*": path.resolve(__dirname, "./utils"),
+      "@hooks/*": path.resolve(__dirname, "./hooks"),
+    },
+  },
 })
